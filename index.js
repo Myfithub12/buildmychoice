6099248eb0d7c0b7c6c38be5d1c82b9fe68eed70
Mark Hollrah <mhollrah0001@gmail.com>
2025-11-04 18:31:38 -0500

    Build My Choice Project

---
+++ index.js
@@ -0,0 +1,9 @@
+const person = new Object();
+    person.name = "Mark Hollrah",
+    person.age = 48,
+    person.job = "Unemployed"
+
+    document.getElementById("name").textContent = `Name: ${person.name}`;
+    document.getElementById("age").textContent = `Age: ${person.age}`;
+    document.getElementById("job").textContent = `Job: ${person.job}`;
+
